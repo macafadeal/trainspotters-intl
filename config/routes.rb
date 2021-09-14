b6b151e5abc64c64d89a55ce6e3db49f3f6dcce8
Mateus <macafadeal@gmail.com>
2021-09-14 10:16:01 +0100

    Code refactoring, linting, and minor changes.
    Co-Authored-By: Mateus <macafadeal@gmail.com>

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Rails
+  .application
+  .routes
+  .draw do
+    devise_for :users
+
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    root to: 'pages#home'
+
+    resources :trains, only: %i[new create index show]
+  end
